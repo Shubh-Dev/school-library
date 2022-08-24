0debaf86186bccbdd8358ce582f0955b0487f7e4
Shubh <shubh.scb@gmail.com>
2022-08-24 18:26:37 +0530

    CHANGE AS PER REVIEW

--- person.rb
+++ person.rb
@@ -1,28 +1,23 @@
 class Person
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
-    @name = name
     @age = age
+    @name = name
     @parent_permission = parent_permission
   end
 
-  def _id
-    @id
-  end
+  # get method
+  attr_reader :id
 
-  def _name
-    @name
-  end
-
-  def _age
-    @age
-  end
+  # set method
+  attr_accessor :name, :age
 
   def of_age?
     @age >= 18
   end
+  private: of_age?
 
   def can_use_services?
-    @age >= 18 || @parent_permission
+    of_age? || @parent_permission
   end
 end
