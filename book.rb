bbb03e29c544ec5ba302e7d67b6ff4709a44b5e0
Shubh <shubh.scb@gmail.com>
2022-08-29 20:38:25 +0530

    LINTERS ERR FIX

--- book.rb
+++ book.rb
@@ -1,16 +1,15 @@
 require_relative './rental'
 class Book
+  attr_accessor :title, :author
+  attr_reader :rentals
 
-    attr_accessor :title, :author
-    attr_reader :rentals
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
 
-    def initialize(title,author)
-        @title = title
-        @author = author
-        @rentals = []
-    end
-
-    def add_rental (person, date)
-        Rental.new(date, person, self)
-    end
+  def add_rental(person, date)
+    Rental.new(date, person, self)
+  end
 end
--- rental.rb
+++ rental.rb
@@ -1,12 +1,13 @@
 require_relative './book'
 
-class Rental 
-    attr_accessor :date :person :book
-    def initialize(date, book, person)
-        @date = date
-        @book = book
-        book.rentals << self
-        @person = person
-        person.rentals << self
-    end
+class Rental
+  attr_accessor :date, :person, :book
+
+  def initialize(date, person, book)
+    @date = date
+    @book = book
+    book.rentals << self
+    @person = person
+    person.rentals << self
+  end
 end
