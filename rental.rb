7568f606c11ec021ff090ebe1377fb4b6bdb7ede
Shubh <shubh.scb@gmail.com>
2022-08-29 22:30:04 +0530

    LINTERS ERR FIX

--- rental.rb
+++ rental.rb
@@ -1,7 +1,7 @@
 require_relative './book'
 
 class Rental
-  attr_accessor :date, :person, :book :rentals
+  attr_accessor :date, :person, :book, :rentals
 
   def initialize(date, person, book)
     @rentals = []
@@ -14,17 +14,7 @@ class Rental
     book.rentals << self unless @book.rentals.include?(self)
   end
 
-  def person=(person)
-    @person = person
-    @person.rentals << self unless @person.rentals.include?(self)
+  def add_rental(book, date)
+    Rental.new(date, self, book)
   end
-
-    def book=(book)
-        @book = book
-        @book.rentals << self unless @book.rentals.include?(self)
-    end
-
-    def add_rental(book, date)
-        Rental.new(date, self, book)
-    end 
 end
